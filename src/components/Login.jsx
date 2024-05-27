aa6500c831e958c4f894149971bb683b001387d0
maharanaanil424@gmail.com <maharanaanil424@gmail.com>
2024-05-27 11:52:21 +0530

    this is the 2nd comment

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <p>Please login to access more features.</p>
+    </div>
+  );
+}
+
+export default Login;
